e886516d196dc915742708d1b49941c13978e99c
Soyab Nandhla <5205954@student.gloscol.ac.uk>
2021-05-11 13:17:07 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,27 +12,27 @@ app.get('/', function(req, res) {
   res.render('pages/index');
 });
 
-app.get("/arc-sw.js", function(request, response) {
-  response.sendFile(__dirname + "/arc-sw.js");
+app.get("/arc-sw.js", function(req, res) {
+  res.sendFile(__dirname + "/arc-sw.js");
 });
 
-app.post("/update", function(request, response) {
-  request.on("data", function(chunk) {
+app.post("/update", function(req, res) {
+  req.on("data", function(chunk) {
     let sig =
       "sha1=" +
       crypto
         .createHmac("sha1", process.env.SECRET)
         .update(chunk.toString())
         .digest("hex");
-    if (request.headers["x-hub-signature"] == sig) {
+    if (req.headers["x-hub-signature"] == sig) {
       exec("npm run update");
     }
   });
-  response.end();
+  res.render('pages/index');
 });
 
-app.use(function(request, response) {
-  response.redirect("/");
+app.use(function(req, res) {
+  res.redirect("/");
 });
 
 const listener = app.listen(process.env.PORT, function() {
