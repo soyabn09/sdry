7727dd3b055b4fb15fb0f6fe28335b06cebb2278
Soyab Nandhla <5205954@student.gloscol.ac.uk>
2021-05-11 12:39:05 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const crypto = require('crypto');
+const exec = require('child_process').exec;
+
+app.set('view engine', 'ejs');
+
+app.use(express.static("public"));
+
+// index page
+app.get('/', function(req, res) {
+  res.render('pages/index');
+});
+
+app.get("/arc-sw.js", function(request, response) {
+  response.sendFile(__dirname + "/arc-sw.js");
+});
+
+app.get("/update", function(request, response) {
+  let sig = "sha1=" + crypto.createHmac('sha1', process.env.SECRET).update(chunk.toString()).digest('hex');
+  if (req.headers['x-hub-signature'] == sig) {
+            exec('npm run update');
+        }
+});
+
+app.use(function(request, response) {
+  response.redirect("/");
+});
+
+const listener = app.listen(process.env.PORT, function() {
+  console.log("Your app is listening on port " + listener.address().port);
+});
