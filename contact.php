ae32c400d03bee9b04bc19b5030661291c6e6700
Soyab Nandhla <45386264+soyabn09@users.noreply.github.com>
2021-05-21 09:24:21 +0100

    Replace services with shop

--- contact.php
+++ contact.php
@@ -26,7 +26,7 @@
                             </a>
                         </li>
                         <li class="nav-item"><a class="nav-link" href="/about">About</a></li>
-                        <li class="nav-item"><a class="nav-link" href="/services">Services</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/shop">Shop</a></li>
                         <li class="nav-item active"><a class="nav-link" href="/contact">Contact</a></li>
                     </ul>
                 </div>
--- shop.php
+++ shop.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="A shop template ¯\_(ツ)_/¯" />
         <meta name="author" content="Soyab Nandhla" />
-        <title>SDRY - Services</title>
+        <title>SDRY - Shop</title>
         <!-- Favicon-->
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
         <!-- Core theme CSS (includes Bootstrap)-->
@@ -25,8 +25,8 @@
                                 <span class="sr-only">(current)</span>
                             </a>
                         </li>
+                        <li class="nav-item active"><a class="nav-link" href="/shop">Shop</a></li>
                         <li class="nav-item"><a class="nav-link" href="/about">About</a></li>
-                        <li class="nav-item active"><a class="nav-link" href="/services">Services</a></li>
                         <li class="nav-item"><a class="nav-link" href="/contact">Contact</a></li>
                     </ul>
                 </div>
