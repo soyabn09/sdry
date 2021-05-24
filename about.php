6fd81b582c59db1578be91da7b11e4de7dce173c
Soyab Nandhla <45386264+soyabn09@users.noreply.github.com>
2021-05-24 18:37:22 +0100

    idk

--- about.php
+++ about.php
@@ -32,60 +32,29 @@
                 </div>
             </div>
         </nav>
-        <!-- Header -->
-        <header class="bg-primary text-center py-5 mb-4">
-        <div class="container">
-            <h1 class="font-weight-light text-white">Meet the Team</h1>
-        </div>
-        </header>
-
-        <!-- Page Content -->
-        <div class="container1 mt-5">
-        <div class="row mt-5 mb-5 mx-5">
-            <!-- Team Member 1 -->
-            <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-0 shadow">
-                <img src="https://source.unsplash.com/TMgQMXoglsM/500x350" class="card-img-top" alt="...">
-                <div class="card-body text-center">
-                <h5 class="card-title mb-0">Team Member</h5>
-                <div class="card-text text-black-50">Web Developer</div>
+        <!-- Main Navigation -->
+        <!-- Main layout -->
+        <main>
+            <div class="container mt-4 mb-5">
+                <h3 class="text-center py-3 mb-5">A company that strives on making their clients happy and geting paid.</h3>
+                <!-- Grid row -->
+                <div class="row">
+                    <!-- Grid column -->
+                    <div class="col-lg-5">
+                        <img class="img-fluid rounded z-depth-2" src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/new/img(21).jpg" alt="sample">
+                    </div>
+                    <!-- Grid column -->
+                    <!-- Grid column -->
+                    <div class="col-lg-7">
+                        <p class="mt-4 mt-lg-0">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio molestiae sit voluptates assumenda omnis dolorem porro non placeat natus iusto modi, reprehenderit voluptatum ducimus ab unde ad quis amet soluta. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur.</p>
+                    </div>
+                    <!-- Grid column -->
                 </div>
+            <!-- Grid row -->
             </div>
-            </div>
-            <!-- Team Member 2 -->
-            <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-0 shadow">
-                <img src="https://source.unsplash.com/9UVmlIb0wJU/500x350" class="card-img-top" alt="...">
-                <div class="card-body text-center">
-                <h5 class="card-title mb-0">Team Member</h5>
-                <div class="card-text text-black-50">Web Developer</div>
-                </div>
-            </div>
-            </div>
-            <!-- Team Member 3 -->
-            <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-0 shadow">
-                <img src="https://source.unsplash.com/sNut2MqSmds/500x350" class="card-img-top" alt="...">
-                <div class="card-body text-center">
-                <h5 class="card-title mb-0">Team Member</h5>
-                <div class="card-text text-black-50">Web Developer</div>
-                </div>
-            </div>
-            </div>
-            <!-- Team Member 4 -->
-            <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-0 shadow">
-                <img src="https://source.unsplash.com/ZI6p3i9SbVU/500x350" class="card-img-top" alt="...">
-                <div class="card-body text-center">
-                <h5 class="card-title mb-0">Team Member</h5>
-                <div class="card-text text-black-50">Web Developer</div>
-                </div>
-            </div>
-            </div>
-        </div>
-        <!-- /.row -->
-        </div>
-        <!-- /.container -->
+        </main>
+        <!-- Main layout -->
         <!-- Footer-->
         <?php include 'functions/copyright.php' ?>
         <footer class="py-5 bg-dark">
