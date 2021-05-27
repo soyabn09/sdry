339bb789240cbda00cb0baead724de54175480f7
Soyab Nandhla <45386264+soyabn09@users.noreply.github.com>
2021-05-27 11:59:57 +0100

    prettify contact

--- contact.php
+++ contact.php
@@ -1,108 +1,176 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="SDRY Company ¯\_(ツ)_/¯" />
-        <meta name="author" content="Soyab Nandhla" />
-        <title>SDRY - Contact</title>
-        <!-- Favicon-->
-        <link rel="icon" type="image/x-icon" href="/assets/favicon.ico" />
-        <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="/css/styles.css" rel="stylesheet" />
-        <script async src="https://arc.io/widget.min.js#BX2apmLr"></script>
-    </head>
-    <body>
-        <!-- Navigation-->
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container">
-                <a class="navbar-brand" href="/">SDRY</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="/shop">Shop</a></li>
-                        <li class="nav-item"><a class="nav-link" href="/about">About</a></li>
-                        <li class="nav-item active"><a class="nav-link" href="/contact">Contact</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <!-- Page Content-->
-        <div class="container mt-5 mb-5">
-            <div class="row mt-5">
-                <!-- Section Titile -->
-                <div class="col-md-12 wow animated fadeInLeft" data-wow-delay=".2s">
-                    <h1 class="section-title">Love to Hear From You!</h1>
-                </div>
-            </div>
-            <div class="row">
-                <!-- Section Titile -->
-                <div class="col-md-6 mt-3 contact-widget-section2 wow animated fadeInLeft" data-wow-delay=".2s">
-                    <p>You are free to contact us by email, phone or this form. We will be in the office and respond to message during our office availability times.</p>
-
-                    <div class="find-widget">
-                    Company: SDRY
-                    </div>
-                    <div class="find-widget">
-                    Address: 10 Downing Street, London, UK
-                    </div>
-                    <div class="find-widget">
-                    Phone: <a href="tel:+442079460985">+44 20 7946 0985</a>
-                    </div>
-                    <div class="find-widget">
-                    Website: <a href="https://sdry.online">sdry.online</a>
-                    </div>
-                    <div class="find-widget">
-                    Available: Mon to Fri: 08:00 AM - 10:00 PM
-                    </div>
-                </div>
-                <!-- contact form -->
-                <div class="col-md-6 wow animated fadeInRight" data-wow-delay=".2s">
-                    <form class="shake" role="form" method="post" id="contactForm" name="contact-form" data-toggle="validator">
-                        <!-- Name -->
-                        <div class="form-group label-floating">
-                            <label class="control-label" for="name">Name</label>
-                            <input class="form-control" id="name" type="text" name="name" required data-error="Please enter your name">
-                            <div class="help-block with-errors"></div>
-                        </div>
-                        <!-- email -->
-                        <div class="form-group label-floating">
-                            <label class="control-label" for="email">Email</label>
-                            <input class="form-control" id="email" type="email" name="email" required data-error="Please enter your Email">
-                            <div class="help-block with-errors"></div>
-                        </div>
-                        <!-- Subject -->
-                        <div class="form-group label-floating">
-                            <label class="control-label">Subject</label>
-                            <input class="form-control" id="msg_subject" type="text" name="subject" required data-error="Please enter your message subject">
-                            <div class="help-block with-errors"></div>
-                        </div>
-                        <!-- Message -->
-                        <div class="form-group label-floating">
-                            <label for="message" class="control-label">Message</label>
-                            <textarea class="form-control" rows="3" id="message" name="message" required data-error="Write your message"></textarea>
-                            <div class="help-block with-errors"></div>
-                        </div>
-                        <!-- Form Submit -->
-                        <div class="form-submit mt-5">
-                            <button class="btn border-secondary" type="submit" id="form-submit"><i class="material-icons mdi mdi-message-outline"></i> Send Message</button>
-                            <div id="msgSubmit" class="h3 text-center hidden"></div>
-                            <div class="clearfix"></div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+  <head>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <meta name="description" content="SDRY Company ¯\_(ツ)_/¯" />
+    <meta name="author" content="Soyab Nandhla" />
+    <title>SDRY - Contact</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="/assets/favicon.ico" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="/css/styles.css" rel="stylesheet" />
+    <script async src="https://arc.io/widget.min.js#BX2apmLr"></script>
+  </head>
+  <body>
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="/">SDRY</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarResponsive"
+          aria-controls="navbarResponsive"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="/shop">Shop</a></li>
+            <li class="nav-item">
+              <a class="nav-link" href="/about">About</a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="/contact">Contact</a>
+            </li>
+          </ul>
         </div>
-        <!-- Footer-->
-        <?php include 'functions/copyright.php' ?>
-        <footer class="py-5 bg-dark">
-            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; SDRY <?php auto_copyright("1979"); ?></p></div>
-        </footer>
-        <!-- Bootstrap core JS-->
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
-        <!-- Core theme JS-->
-        <script src="/js/scripts.js"></script>
-    </body>
+      </div>
+    </nav>
+    <!-- Page Content-->
+    <div class="container mt-5 mb-5">
+      <div class="row mt-5">
+        <!-- Section Titile -->
+        <div class="col-md-12 wow animated fadeInLeft" data-wow-delay=".2s">
+          <h1 class="section-title">Love to Hear From You!</h1>
+        </div>
+      </div>
+      <div class="row">
+        <!-- Section Titile -->
+        <div
+          class="col-md-6 mt-3 contact-widget-section2 wow animated fadeInLeft"
+          data-wow-delay=".2s"
+        >
+          <p>
+            You are free to contact us by email, phone or this form. We will be
+            in the office and respond to message during our office availability
+            times.
+          </p>
+
+          <div class="find-widget">
+            Company: SDRY
+          </div>
+          <div class="find-widget">
+            Address: 10 Downing Street, London, UK
+          </div>
+          <div class="find-widget">
+            Phone: <a href="tel:+442079460985">+44 20 7946 0985</a>
+          </div>
+          <div class="find-widget">
+            Website: <a href="https://sdry.online">sdry.online</a>
+          </div>
+          <div class="find-widget">
+            Available: Mon to Fri: 08:00 AM - 10:00 PM
+          </div>
+        </div>
+        <!-- contact form -->
+        <div class="col-md-6 wow animated fadeInRight" data-wow-delay=".2s">
+          <form
+            class="shake"
+            role="form"
+            method="post"
+            id="contactForm"
+            name="contact-form"
+            data-toggle="validator"
+          >
+            <!-- Name -->
+            <div class="form-group label-floating">
+              <label class="control-label" for="name">Name</label>
+              <input
+                class="form-control"
+                id="name"
+                type="text"
+                name="name"
+                required
+                data-error="Please enter your name"
+              />
+              <div class="help-block with-errors"></div>
+            </div>
+            <!-- email -->
+            <div class="form-group label-floating">
+              <label class="control-label" for="email">Email</label>
+              <input
+                class="form-control"
+                id="email"
+                type="email"
+                name="email"
+                required
+                data-error="Please enter your Email"
+              />
+              <div class="help-block with-errors"></div>
+            </div>
+            <!-- Subject -->
+            <div class="form-group label-floating">
+              <label class="control-label">Subject</label>
+              <input
+                class="form-control"
+                id="msg_subject"
+                type="text"
+                name="subject"
+                required
+                data-error="Please enter your message subject"
+              />
+              <div class="help-block with-errors"></div>
+            </div>
+            <!-- Message -->
+            <div class="form-group label-floating">
+              <label for="message" class="control-label">Message</label>
+              <textarea
+                class="form-control"
+                rows="3"
+                id="message"
+                name="message"
+                required
+                data-error="Write your message"
+              ></textarea>
+              <div class="help-block with-errors"></div>
+            </div>
+            <!-- Form Submit -->
+            <div class="form-submit mt-5">
+              <button
+                class="btn border-secondary"
+                type="submit"
+                id="form-submit"
+              >
+                <i class="material-icons mdi mdi-message-outline"></i> Send
+                Message
+              </button>
+              <div id="msgSubmit" class="h3 text-center hidden"></div>
+              <div class="clearfix"></div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+    <!-- Footer-->
+    <?php include 'functions/copyright.php' ?>
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; SDRY <?php auto_copyright("1979"); ?></p>
+      </div>
+    </footer>
+    <!-- Bootstrap core JS-->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="/js/scripts.js"></script>
+  </body>
 </html>
