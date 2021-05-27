f528067b8812bf1cc9834d69c853085095791fb6
Soyab Nandhla <45386264+soyabn09@users.noreply.github.com>
2021-05-27 12:01:10 +0100

    prettify about

--- about.php
+++ about.php
@@ -1,66 +1,123 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="SDRY Company ¯\_(ツ)_/¯" />
-        <meta name="author" content="Soyab Nandhla" />
-        <title>SDRY - About</title>
-        <!-- Favicon-->
-        <link rel="icon" type="image/x-icon" href="/assets/favicon.ico" />
-        <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="/css/styles.css" rel="stylesheet" />
-        <script async src="https://arc.io/widget.min.js#BX2apmLr"></script>
-    </head>
-    <body>
-        <!-- Navigation-->
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container">
-                <a class="navbar-brand" href="/">SDRY</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="/shop">Shop</a></li>
-                        <li class="nav-item"><a class="nav-link" href="/about">About</a></li>
-                        <li class="nav-item"><a class="nav-link" href="/contact">Contact</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <!-- Main Navigation -->
-        <!-- Main layout -->
-        <main>
-            <div class="container mt-4 mb-5">
-                <h3 class="text-center py-3 mb-5">A company that strives on making their clients happy and geting paid.</h3>
-                <!-- Grid row -->
-                <div class="row">
-                    <!-- Grid column -->
-                    <div class="col-lg-5">
-                        <img class="img-fluid rounded z-depth-2" src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/new/img(21).jpg" alt="sample">
-                    </div>
-                    <!-- Grid column -->
-                    <!-- Grid column -->
-                    <div class="col-lg-7">
-                        <p>This is not us and not our company but who cares anyway. This text is here for no reason at all other than making it look like there is a beauty about the page being here. This so-called company makes 0 $,£,€ or whatever currency you use in whatever place you live at. Btw we use DNW (Do no work) currency because that is what I intend to do.</p>
-                        <p>More random nonsense to type here and so let me just see what I can do about it. Ah, I have an idea let's get my auto-correct feature on my phone to type some random words into this paragraph or even better idea get the full bee story from the web and somehow paste it in here without causing issues as I do not want to fix issues with this paragraph as I would be here all day writing up the perfect bee story by fixing the error that HTML should learn to fix all by itself. I could also get someone to write about how I will be doing those things for free, and no that is not free labour but what I would call it is um ah who cares free labour it is.</p>
-                        <p>This is a story about Donald not Donald Trump but Donald Richeese yes um so he once went off to somewhere land and came back from uh some-other-place land anyways he then did some work at the wowowowow company and then went and made sdry and some stupid company if you ask me and he did this back in 1979 when there were shoes? IDK what this story is but that is our boss and the boss of the company we are working at and plz help us. We have to type random stuff into this website and I don't like it anymore and I want to go home. I have been here since I was 21 and now that I am 22 I feel like I am getting old. If you are reading pls donate to the Get People Out Of SDRY Company Because They Are Scared non-profit organization also known as (GPOOSCBTAS).</p>
-                    </div>
-                    <!-- Grid column -->
-                </div>
-            <!-- Grid row -->
-            </div>
-        </main>
-        <!-- Main layout -->
-        <!-- Footer-->
-        <?php include 'functions/copyright.php' ?>
-        <footer class="py-5 bg-dark">
-            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; SDRY <?php auto_copyright("1979"); ?></p></div>
-        </footer>
-        <!-- Bootstrap core JS-->
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
-        <!-- Core theme JS-->
-        <script src="/js/scripts.js"></script>
-    </body>
+  <head>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <meta name="description" content="SDRY Company ¯\_(ツ)_/¯" />
+    <meta name="author" content="Soyab Nandhla" />
+    <title>SDRY - About</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="/assets/favicon.ico" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="/css/styles.css" rel="stylesheet" />
+    <script async src="https://arc.io/widget.min.js#BX2apmLr"></script>
+  </head>
+  <body>
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="/">SDRY</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarResponsive"
+          aria-controls="navbarResponsive"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="/shop">Shop</a></li>
+            <li class="nav-item">
+              <a class="nav-link" href="/about">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- Main Navigation -->
+    <!-- Main layout -->
+    <main>
+      <div class="container mt-4 mb-5">
+        <h3 class="text-center py-3 mb-5">
+          A company that strives on making their clients happy and geting paid.
+        </h3>
+        <!-- Grid row -->
+        <div class="row">
+          <!-- Grid column -->
+          <div class="col-lg-5">
+            <img
+              class="img-fluid rounded z-depth-2"
+              src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/new/img(21).jpg"
+              alt="sample"
+            />
+          </div>
+          <!-- Grid column -->
+          <!-- Grid column -->
+          <div class="col-lg-7">
+            <p>
+              This is not us and not our company but who cares anyway. This text
+              is here for no reason at all other than making it look like there
+              is a beauty about the page being here. This so-called company
+              makes 0 $,£,€ or whatever currency you use in whatever place you
+              live at. Btw we use DNW (Do no work) currency because that is what
+              I intend to do.
+            </p>
+            <p>
+              More random nonsense to type here and so let me just see what I
+              can do about it. Ah, I have an idea let's get my auto-correct
+              feature on my phone to type some random words into this paragraph
+              or even better idea get the full bee story from the web and
+              somehow paste it in here without causing issues as I do not want
+              to fix issues with this paragraph as I would be here all day
+              writing up the perfect bee story by fixing the error that HTML
+              should learn to fix all by itself. I could also get someone to
+              write about how I will be doing those things for free, and no that
+              is not free labour but what I would call it is um ah who cares
+              free labour it is.
+            </p>
+            <p>
+              This is a story about Donald not Donald Trump but Donald Richeese
+              yes um so he once went off to somewhere land and came back from uh
+              some-other-place land anyways he then did some work at the
+              wowowowow company and then went and made sdry and some stupid
+              company if you ask me and he did this back in 1979 when there were
+              shoes? IDK what this story is but that is our boss and the boss of
+              the company we are working at and plz help us. We have to type
+              random stuff into this website and I don't like it anymore and I
+              want to go home. I have been here since I was 21 and now that I am
+              22 I feel like I am getting old. If you are reading pls donate to
+              the Get People Out Of SDRY Company Because They Are Scared
+              non-profit organization also known as (GPOOSCBTAS).
+            </p>
+          </div>
+          <!-- Grid column -->
+        </div>
+        <!-- Grid row -->
+      </div>
+    </main>
+    <!-- Main layout -->
+    <!-- Footer-->
+    <?php include 'functions/copyright.php' ?>
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; SDRY <?php auto_copyright("1979"); ?></p>
+      </div>
+    </footer>
+    <!-- Bootstrap core JS-->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="/js/scripts.js"></script>
+  </body>
 </html>
