994f0587e754dc9a013a8b5f9738a40e908e6993
Soyab Nandhla <45386264+soyabn09@users.noreply.github.com>
2021-06-10 10:35:27 +0100

    prettify

--- functions/copyright.php
+++ functions/copyright.php
@@ -1,15 +1,13 @@
 <?php function auto_copyright($year = 'auto')
-  { ?>
-    <?php if (intval($year) == 'auto') {
-      $year = date('Y');
-    } ?>
-    <?php if (intval($year) == date('Y')) {
-      echo intval($year);
-    } ?>
-    <?php if (intval($year) < date('Y')) {
-      echo intval($year) . ' - ' . date('Y');
-    } ?>
-    <?php if (intval($year) > date('Y')) {
-      echo date('Y');
-    } ?>
-  <?php } ?>
+  {
+    if (intval($year) == 'auto') {
+          $year = date('Y');
+        }
+    if (intval($year) == date('Y')) {
+          echo intval($year);
+        }
+    if (intval($year) < date('Y')) {
+          echo intval($year) . ' - ' . date('Y');
+        }
+    if (intval($year) >
+date('Y')) { echo date('Y'); } } ?>
